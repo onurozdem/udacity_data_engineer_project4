912f1f46c39f23977ce591939a904b648d50db61
Onur Özdemir <onurozdemir94@hotmail.com>
2021-01-21 02:58:47 +0300

    Update etl.py

--- etl.py
+++ etl.py
@@ -103,8 +103,8 @@ def process_log_data(spark, input_data, output_data):
 
 def main():
     spark = create_spark_session()
-    input_data = "s3a://uda-tupras/"
-    output_data = "s3a://uda-tupras/project4-out/"
+    input_data = "s3a://udacity-dend/"
+    output_data = "s3a://udacity-dend/project4-out/"
     
     process_song_data(spark, input_data, output_data)    
     process_log_data(spark, input_data, output_data)
